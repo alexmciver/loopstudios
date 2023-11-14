dfb07bf0ca7440d594512e1c6f56a38dfbe5c8ab
Alex McIver <alexmciver@ymail.com>
2023-11-14 17:33:17 +0000

    Finished image left text right

--- wp-content/themes/loop-studios-theme/template-parts/image-left-text-right/image-left-text-right.php
+++ wp-content/themes/loop-studios-theme/template-parts/image-left-text-right/image-left-text-right.php
@@ -1,8 +1,24 @@
+<?php
+ $image_left = get_field('image_left');
+ $right_title_text = get_field('right_title_text');
+ $right_body_copy = get_field('right_body_copy');
+?>
+
+
 <div class="image-left-text-right-block">
  <div class="container">
   <div class="flexbox">
-   <div class="content-left"></div>
-   <div class="content-right"></div>
+   <div class="content-left">
+    <div class="image">
+     <img src="<?php echo esc_url($image_left); ?>" alt="">
+    </div>
+   </div>
+   <div class="content-right">
+    <div class="text-block">
+     <h2><?php echo esc_html($right_title_text); ?></h2>
+     <p><?php echo esc_html($right_body_copy); ?></p>
+    </div>
+   </div>
   </div>
  </div>
 </div>
