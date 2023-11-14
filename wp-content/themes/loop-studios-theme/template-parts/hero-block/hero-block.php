cacf23e1f8d040411ee842940905214de5d60bbe
Alex McIver <alexmciver@ymail.com>
2023-11-14 17:33:54 +0000

    Made hero text dynamic

--- wp-content/themes/loop-studios-theme/template-parts/hero-block/hero-block.php
+++ wp-content/themes/loop-studios-theme/template-parts/hero-block/hero-block.php
@@ -1,11 +1,12 @@
 <?php
-$hero_background_image = get_field('hero_background_image');
+ $hero_background_image = get_field('hero_background_image');
+ $hero_text = get_field('hero_text');
 ?>
 
-<div class="hero-block" style="background-image: url('<?php echo esc_url($hero_background_image); ?>');">
+<div class="hero-block" style="background-image: url('<?php echo esc_attr($hero_background_image); ?>');">
  <div class="container">
   <div class="hero-block-content">
-   <h1>Immersive experiences that deliver</h1>
+   <h1><?php echo esc_html($hero_text); ?></h1>
   </div>
  </div>
 </div>
