4c202243eb69a29ee4a9608640b2e9492814d195
Alex McIver <alexmciver@ymail.com>
2023-11-27 20:18:49 +0000

    Header tweaks

--- wp-content/themes/loop-studios-theme/header.php
+++ wp-content/themes/loop-studios-theme/header.php
@@ -1,6 +1,5 @@
 <?php
- $logo_image = get_field('logo_image', 'option');
- $hamburger_image = get_field('hamburger_image','option');
+ $logo_image = get_field('logo_image', 'option'); 
 ?>
 <div class="header">
  <div class="container">
@@ -19,8 +18,28 @@
         ?>
    </nav>
    <button class="menu-icon" id="menu-toggle">
-    <img src="<?php echo esc_url($hamburger_image); ?>" alt="Menu">
+    <svg width="24" height="16" xmlns="http://www.w3.org/2000/svg">
+     <g fill="#FFF" fill-rule="evenodd">
+      <path d="M0 0h24v2H0zM0 7h24v2H0zM0 14h24v2H0z" />
+     </g>
+    </svg>
    </button>
+   <div class="mobile-menu">
+    <div class="container">
+     <div class="logo-and-close-button">
+      <div class="logo">
+       <img src="<?php echo esc_url($logo_image); ?>" alt="">
+      </div>
+      <div class="close-button">
+       <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
+        <path
+         d="M17.778.808l1.414 1.414L11.414 10l7.778 7.778-1.414 1.414L10 11.414l-7.778 7.778-1.414-1.414L8.586 10 .808 2.222 2.222.808 10 8.586 17.778.808z"
+         fill="#FFF" fill-rule="evenodd" />
+       </svg>
+      </div>
+     </div>
+    </div>
+   </div>
   </div>
  </div>
 </div>
